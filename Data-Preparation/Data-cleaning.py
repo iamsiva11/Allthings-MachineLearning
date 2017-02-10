76218dc8fb8c0c13ea90f0286e60137178d493cb
Siva Sundaramoorthy <siva11.sundaramoorthy@gmail.com>
2017-02-10 14:04:45 +0530

    fixed typo

--- Data-Preparation/Data-cleaning.py
+++ Data-Preparation/Data-cleaning.py
@@ -1,3 +1,8 @@
+"""
+Convert Types
+Delete a column
+Handling Nan Values - Delete rows with NaN values, Mark value as NaN
+"""
 import pandas as pd
 import numpy as np
 
@@ -18,13 +23,13 @@ print dataset.head(20)
 #dataset2 - breast-cancer-wisconsin
 url = "https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/breast-cancer-wisconsin.data"
 names = ['Code', 'Clump-Thickness', 'Cell-Size', 'Cell-Shape', 'Adhesion', 'Single-Cell-Size', 'Bare-Nuclei', 'Chromatin', 'Nucleoli', 'Mitoses', 'Class']
-dataset = pandas.read_csv(url, names=names)
+dataset = pd.read_csv(url, names=names)
 
 # Handling Nan Values
 # Mark value as NaN
-print(pandas.unique(dataset['Bare-Nuclei']))
+print(pd.unique(dataset['Bare-Nuclei']))
 dataset[['Bare-Nuclei']] = dataset[['Bare-Nuclei']].replace('?', numpy.NaN)
-print(pandas.unique(dataset['Bare-Nuclei']))
+print(pd.unique(dataset['Bare-Nuclei']))
 
 # Delete rows with NaN values
 dataset[['Bare-Nuclei']] = dataset[['Bare-Nuclei']].replace('?', numpy.NaN)
