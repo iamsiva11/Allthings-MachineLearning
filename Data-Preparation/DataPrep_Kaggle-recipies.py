aafc3eb7b47e61503b9813d2caade58646fe5eac
Siva Sundaramoorthy <siva11.sundaramoorthy@gmail.com>
2017-02-14 17:49:26 +0530

    drop cols, copy data

---
+++ Data-Preparation/DataPrep_Kaggle-recipies.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+
+#Pimas data
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/pima-indians-diabetes/pima-indians-diabetes.data"
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+dataframe = pandas.read_csv(url, names=names)
+
+#Backup training data
+train_old = dataframe.copy()
+
+#Drop columns
+dataframe = dataframe.drop(['skin',], axis=1)  
+#dataframe.head(10)
+
+
+
+
